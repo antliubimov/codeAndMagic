851312c6eedeea5e088825ce4c2867b168425ddc
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-05 13:25:54 +0500

    done js/colorize module

--- js/setup.js
+++ js/setup.js
@@ -1,43 +1,34 @@
 // setup.js
 'use strict';
-var ESC_KEY = 'Escape';
-var ENTER_KEY = 'Enter';
 
 var firstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
 
 var similarListElement = document.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var getRandomElement = function (arr) {
-  var rand = Math.random() * arr.length - 0.5;
-  return arr[Math.round(rand)];
-};
-
 var wizards = [
   {
-    name: `${getRandomElement(firstName)} ${getRandomElement(lastName)}`,
-    coatColor: `${getRandomElement(coatColor)}`,
-    eyesColor: `${getRandomElement(eyesColor)}`
+    name: `${window.colorize.getRandomColor(firstName)} ${window.colorize.getRandomColor(lastName)}`,
+    coatColor: `${window.colorize.getRandomColor(window.colorize.wizardCoatColor)}`,
+    eyesColor: `${window.colorize.getRandomColor(window.colorize.wizardEyesColor)}`
   },
   {
-    name: `${getRandomElement(firstName)} ${getRandomElement(lastName)}`,
-    coatColor: `${getRandomElement(coatColor)}`,
-    eyesColor: `${getRandomElement(eyesColor)}`
+    name: `${window.colorize.getRandomColor(firstName)} ${window.colorize.getRandomColor(lastName)}`,
+    coatColor: `${window.colorize.getRandomColor(window.colorize.wizardCoatColor)}`,
+    eyesColor: `${window.colorize.getRandomColor(window.colorize.wizardEyesColor)}`
   },
   {
-    name: `${getRandomElement(firstName)} ${getRandomElement(lastName)}`,
-    coatColor: `${getRandomElement(coatColor)}`,
-    eyesColor: `${getRandomElement(eyesColor)}`
+    name: `${window.colorize.getRandomColor(firstName)} ${window.colorize.getRandomColor(lastName)}`,
+    coatColor: `${window.colorize.getRandomColor(window.colorize.wizardCoatColor)}`,
+    eyesColor: `${window.colorize.getRandomColor(window.colorize.wizardEyesColor)}`
   },
   {
-    name: `${getRandomElement(firstName)} ${getRandomElement(lastName)}`,
-    coatColor: `${getRandomElement(coatColor)}`,
-    eyesColor: `${getRandomElement(eyesColor)}`
+    name: `${window.colorize.getRandomColor(firstName)} ${window.colorize.getRandomColor(lastName)}`,
+    coatColor: `${window.colorize.getRandomColor(window.colorize.wizardCoatColor)}`,
+    eyesColor: `${window.colorize.getRandomColor(window.colorize.wizardEyesColor)}`
   }
 ];
 
@@ -57,7 +48,11 @@ for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
+// temp
+document.querySelector('.setup-similar').classList.remove('hidden');
+//
 
+var setup = document.querySelector('.setup');
 
 var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
 var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
@@ -66,35 +61,11 @@ var inputCoatColor = document.querySelector('input[name=coat-color]');
 var inputEyesColor = document.querySelector('input[name=eyes-color]');
 var inputFireballColor = document.querySelector('input[name=fireball-color]');
 
-var onWizardColor = function (wizardThing, thingColors) {
-  var randomColor = getRandomElement(thingColors);
+window.colorize.setColor(wizardCoat, inputCoatColor, window.colorize.wizardCoatColor);
+window.colorize.setColor(wizardEyes, inputEyesColor, window.colorize.wizardEyesColor);
+window.colorize.setColor(wizardFireball, inputFireballColor, window.colorize.wizardFireballColor);
 
-  while (randomColor === wizardThing.style.fill) {
-    randomColor = getRandomElement(thingColors);
-  }
-
-  if (wizardThing === wizardFireball) {
-    wizardThing.style.backgroundColor = randomColor;
-  } else {
-    wizardThing.style.fill = randomColor;
-  }
-
-  switch (wizardThing) {
-    case wizardCoat:
-      inputCoatColor.value = randomColor;
-      break;
-    case wizardEyes:
-      inputEyesColor.value = randomColor;
-      break;
-    case wizardFireball:
-      inputFireballColor.value = randomColor;
-      break;
-  }
-};
-
-wizardCoat.addEventListener('click', onWizardColor.bind(null, wizardCoat, coatColor));
-wizardEyes.addEventListener('click', onWizardColor.bind(null, wizardEyes, eyesColor));
-wizardFireball.addEventListener('click', onWizardColor.bind(null, wizardFireball, fireballColor));
+// dialog-handle.js
 
 var dialogHandle = setup.querySelector('.upload');
 
