15fa7fb8047ac5dc3422f5d196ab479892674653
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-05 08:30:25 +0500

    done drag-n-drop from dialog

--- js/setup.js
+++ js/setup.js
@@ -137,3 +137,54 @@ var onWizardColor = function (wizardThing, thingColors) {
 wizardCoat.addEventListener('click', onWizardColor.bind(null, wizardCoat, coatColor));
 wizardEyes.addEventListener('click', onWizardColor.bind(null, wizardEyes, eyesColor));
 wizardFireball.addEventListener('click', onWizardColor.bind(null, wizardFireball, fireballColor));
+
+var dialogHandle = setup.querySelector('.upload');
+
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY,
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        dialogHandle.removeEventListener('click', onClickPreventDefault)
+      };
+      dialogHandle.addEventListener('click', onClickPreventDefault);
+    }
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
