3dcd8072d8faa2c14404fc6e30020f1fd0397bbd
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-05 13:31:48 +0500

    done dialog-handle.js module

--- js/colorize.js
+++ js/colorize.js
@@ -1,5 +1,6 @@
 // colorize.js
 'use strict';
+
 (function () {
   var wizardCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var wizardEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
