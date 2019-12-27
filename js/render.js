726c836982bb258fbe63fa176acae173ef1ac409
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-27 14:02:15 +0500

    done js/render.js

---
+++ js/render.js
@@ -0,0 +1,20 @@
+// render.js
+'use strict';
+
+(function () {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+  return window.render = {
+    renderWizard
+  };
+})();
