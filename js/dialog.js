8ceb4ae19d617e48ec4d6b98e1563647b6266cc4
Anton Liubimov <ant.liubimov@gmail.com>
2020-01-15 15:50:24 +0500

    move all into IIFE

--- js/dialog.js
+++ js/dialog.js
@@ -1,40 +1,41 @@
 // dialog.js
 'use strict';
 
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = document.querySelector('.setup-close');
-var setupUserName = document.querySelector('.setup-user-name');
-//var setupSubmit = document.querySelector('.setup-submit');
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = document.querySelector('.setup-close');
+  var setupUserName = document.querySelector('.setup-user-name');
 
-var onPopupEscPress = function (evt) {
-  if (setupUserName !== document.activeElement) {
-    window.util.isEscEvent(evt, closePopup);
-  }
-};
+  var onPopupEscPress = function (evt) {
+    if (setupUserName !== document.activeElement) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
 
-var openPopup = function () {
-  setup.classList.remove('hidden');
-  document.addEventListener('keydown', onPopupEscPress);
-};
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
 
-var closePopup = function () {
-  setup.classList.add('hidden');
-  document.removeEventListener('keydown', onPopupEscPress);
-};
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
 
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
 
-setupOpen.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, openPopup);
-});
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
 
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
 
-setupClose.addEventListener('keydown', function (evt) {
-  window.util.isEnterEvent(evt, closePopup);
-});
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+})();
